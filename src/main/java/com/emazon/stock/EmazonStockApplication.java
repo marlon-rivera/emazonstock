65ef620fe0908edf7548f5d738747bcaa359a564
marlon-rivera <marlon.rivera@uptc.edu.co>
2024-08-24 07:54:59 -0500

    Estructura de carpetas.

---
+++ src/main/java/com/emazon/stock/EmazonStockApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonStockApplication.class, args);
+	}
+
+}
